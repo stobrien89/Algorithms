d2ca477fd0bb9e27e7ec4df06fe75d86e30cca9f
stobrien89 <stobrien@crimson.ua.edu>
2021-10-16 15:04:26 -0700

    sketch out SLL stack

---
+++ data_structures/stack_sll.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null; 
+        this.size = 0;
+    }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.first) {
+            this.first =  newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    pop() {
+        if (!this.first) return null;
+        let temp = this.first;
+
+        if (this.first === this.last) {
+            this.last = null;
+        }
+
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+
+
+ 
